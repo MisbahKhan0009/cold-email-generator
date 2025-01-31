c01b4c3196d02ee9404eebbbbd4226dace0c34ac
Misbah Khan <misbah.khan@northsouth.edu>
2025-01-31 20:41:43 +0600

    🐛 bug fixed
    Co-authored-by: Misbah Khan <misbah.khan@northsouth.edu>

--- utils.py
+++ utils.py
@@ -9,10 +9,10 @@ def clean_text(text):
         r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', text)
     
     text = re.sub(r'[^a-zA-Z0-9 ]', '', text)
-    # Replace multiple spaces with a single space
+    
     text = re.sub(r'\s{2,}', ' ', text)
   
     text = text.strip()
-    # Remove extra whitespace
+    
     text = ' '.join(text.split())
     return text
