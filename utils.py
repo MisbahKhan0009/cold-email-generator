c92c17c02ba51e9157aaab85a43d478f968a2412
Misbah Khan <misbah.khan@northsouth.edu>
2025-01-31 20:38:57 +0600

    🐛 bug fixed

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ def clean_text(text):
     # Remove URLs
     text = re.sub(
         r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', text)
-    # Remove special characters
+    
     text = re.sub(r'[^a-zA-Z0-9 ]', '', text)
     # Replace multiple spaces with a single space
     text = re.sub(r'\s{2,}', ' ', text)
